9ba5f96f072c99f8a6caede04fb69edcea111347
krraej <34974730+krraej@users.noreply.github.com>
2018-03-03 18:56:37 +0100

    Update blender-script-isometric-camera.py

--- blender-script-isometric-camera.py
+++ blender-script-isometric-camera.py
@@ -21,7 +21,7 @@ class OpenIsometricCameraPanel(View3DPanel, Panel):
     # bl_space_type = 'CLIP_EDITOR'
     bl_label = "Add IsoCamera"
     bl_context = "objectmode"
-    bl_category = "Open IsoCamera" #name of the tab displayed
+    bl_category = "IsoCamera" #name of the tab displayed
     bl_space_type = "VIEW_3D"
     bl_region_type = "UI"
     bl_context = "object"
@@ -29,20 +29,10 @@ class OpenIsometricCameraPanel(View3DPanel, Panel):
     def draw(self, context):
         layout = self.layout
         col = layout.column(align=True)
-        layout.operator("object.camera_add", text="Add Cam", icon="CAMERA_DATA")
+        layout.operator("object.create_isometric_camera", text="Add Cam", icon="CAMERA_DATA")
         
         
-#def register():
-#    bpy.utils.register_module(__name__)
-    
-#def unregister():
- #   bpy.utils.unregister_module(__name__)
-    
-#if __name__ == "__main__":
-#    register()
-    
-
-
+        
 
 class CreateIsometricCamera(bpy.types.Operator):
     """Create Camera with isometric View"""
@@ -80,12 +70,10 @@ class CreateIsometricCamera(bpy.types.Operator):
 
 def register():
     bpy.utils.register_module(__name__)
-    bpy.types.VIEW3D_MT_view.append(menu_func)
 
 def unregister():
     bpy.utils.unregister_module(__name__)
-    bpy.types.VIEW3D_MT_view.remove(menu_func)
 
-def menu_func(self, context):
-    self.layout.operator(CreateIsometricCamera.bl_idname,
-    icon = 'CAMERA_DATA')
+    
+if __name__ == "__main__":
+    register()
